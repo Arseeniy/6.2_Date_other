06cefe2aa9ed89646ae6e08a4645625b179d1d2b
Arseeniy <123qwe.@gmail.com>
2022-12-20 07:10:01 +0400

    Second commit

--- src/com/arseniy/dateother/solution/SolutionClass.java
+++ src/com/arseniy/dateother/solution/SolutionClass.java
@@ -1,21 +1,25 @@
 package com.arseniy.dateother.solution;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SolutionClass {
 
     public static void getCircleSquare(double radius) {
-        double piNumber = 3.14159265359;
+        double piNumber = Math.PI;
         double circleSquare = piNumber * Math.pow(radius, 2);
         System.out.println(String.format("%(.50f", circleSquare));
     }
 
     public static void checkNumber(String initStr) {
-        List<String> stringList = Arrays.asList(initStr.split(" "));
+        List<String> stringList = Arrays.asList(initStr.split( " "));
         double firstNumber = convertNumber(stringList.get(0));
         double secondNumber = convertNumber(stringList.get(1));
         double thirdNumber = convertNumber(stringList.get(2));
@@ -24,17 +28,23 @@ public class SolutionClass {
         } else {
             System.out.println("Третье число НЕ является суммой первых двух.");
         }
+
     }
 
     public static double convertNumber(String strToConvert) {
-        NumberFormat format = NumberFormat.getNumberInstance(new Locale("en", "US"));
+
+        System.out.println(strToConvert);
+        strToConvert = strToConvert.replace(".", ",");
+        NumberFormat format = NumberFormat.getInstance(Locale.getDefault());
         double d = 0;
         try {
             Number number = format.parse(strToConvert);
             d = number.doubleValue();
+            System.out.println(d);
         } catch (ParseException e) {
             e.printStackTrace();
         }
+
         return d;
     }
 
